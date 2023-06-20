6b69f438ca76f2439c4179640fad167601e6186a
kshatriyaCodesf <134126613+kshatriyaCodesf@users.noreply.github.com>
2023-06-20 13:35:25 +0530

    Update lec1PART4.cpp

--- STL library by LUV/vectors/lec1PART4.cpp
+++ STL library by LUV/vectors/lec1PART4.cpp
@@ -31,5 +31,12 @@ return 0;
 toc of following inbuilt function:
 1. v.push_back()->O(1) -- add element inside the vector from the back
 2. v.size()->O(1)      -- gives the size of the element
-3.
+3.vector also has some space complexity like 
+in declaration of one vector it is O(n)
+
+In C++, what are the main functions of vector?
+A vector is a sequence container class that implements dynamic array, which indicates that its size changes as elements are added. A vector holds its items in contiguous memory locations and allocates the memory as needed during execution.
+
+Can we add the elements in the vector from the front?
+Moving all the other parts back means adding to the front of a vector. If you want to perform insertion from front, you should probably use list or deque. 
 */
